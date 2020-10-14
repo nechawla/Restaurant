5889d4f5c7da36052ea9f4ff61f94d55db90ced5
Neelu <neeluchawla84@gmail.com>
2020-10-13 22:01:38 -0400

    Update for contact page

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using Restaurants.Models;
+
+namespace Restaurants.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            using (DBModels dbModels=new DBModels())
+            {
+                return View(dbModels.Contacts.ToList());
+            }
+           
+        }
+
+        // GET: Contact/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Contact/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contact/Create
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (DBModels dbModels=new DBModels())
+                {
+                    dbModels.Contacts.Add(contact);
+                    dbModels.SaveChanges();
+                    MailMessage mailMessage = new MailMessage("noreply.com", contact.email, "Enquiry Confirmation", contact.message);
+                    mailMessage.IsBodyHtml = true;
+                    SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                    client.EnableSsl = true;
+                    client.Credentials = new System.Net.NetworkCredential("bulmaBula@gmail.com", "Test123$");
+                    client.Send(mailMessage);
+
+
+                } 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contact/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Contact/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contact/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Contact/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
